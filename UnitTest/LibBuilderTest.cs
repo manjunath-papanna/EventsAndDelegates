814fe1987990b0bef8fe2f561734bfc8c0570300
manjunath <manjunath.nc@gmail.com>
2016-08-20 18:41:57 +0530

    Added UnitTest Project

---
+++ UnitTest/LibBuilderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventsAndDelegates;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class LibBuilderTest
+    {
+        [TestMethod]
+        public void TestLibBuilder()
+        {
+            var libBuilder = new LibBuilder();
+            Assert.AreEqual(3, libBuilder.Plugins.Count);
+        }
+    }
+}
